1e68b488468dbd63dcc681217cf8dcbcd6eb9df1
Dominoman <olaca@freemail.hu>
2023-10-31 12:49:01 +0100

    Joy config dialog

--- app.py
+++ app.py
@@ -2,33 +2,52 @@ import os.path
 import sys
 from os import rename
 
+from PyQt6 import QtWidgets
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QDialog
 
+from joyconfigdialog import Ui_JoyConfigDialog
 from mainwindow import Ui_MainWindow
 
 MODES = ("", "M6", "MV")
 MODES2 = ("", "TP", "TN")
 JOY = ("", "J1", "J2")
 MOUSE = ("", "P1", "P2")
-DISKMODE = ("", "AD", "RO", "CD")
+DISK_MODE = ("", "AD", "RO", "CD")
 REU = ("", "R5", "R2", "RM")
 
-JOYKEYS = ["JU", "JD", "JL", "JR", "JF", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "A", "B", "C", "D", "E", "F",
-           "G",
-           "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3",
-           "4", "5", "6", "7", "8", "9", "0", "AL",
-           "AU", "CM", "CO", "CT", "CU", "CD", "CL", "CR", "DL", "EN", "HM", "RS", "RE", "SL", "SR", "SS", "SP", "PO"]
-JOYLABELS = ["Joy Up", "Joy Down", "Joy Left", "Joy Right", "Joy Fire", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8",
-             "A", "B", "C", "D", "E",
-             "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-             "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Arrow Left",
-             "Arrow Up", "CBM", ",", "CTRL", "Cursor Up", "Cursor Down", "Cursor Left", "Cursor Right", "Ins/Del",
-             "RETURN", "Clr/Home", "Run/Stop", "RESTORE", "Left Shift", "Right Shift", "Shift Lock", "SPACE",
-             "£"]
+JOY_KEYS = ("JU", "JD", "JL", "JR", "JF", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "A", "B", "C", "D", "E", "F",
+            "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1",
+            "2", "3", "4", "5", "6", "7", "8", "9", "0", "AL", "AU", "CM", "CO", "CT", "CU", "CD", "CL", "CR", "DL",
+            "EN", "HM", "RS", "RE", "SL", "SR", "SS", "SP", "PO", "")
+JOY_LABELS = ("Joy Up", "Joy Down", "Joy Left", "Joy Right", "Joy Fire", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8",
+              "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+              "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Arrow Left", "Arrow Up",
+              "CBM", ",", "CTRL", "Cursor Up", "Cursor Down", "Cursor Left", "Cursor Right", "Ins/Del", "RETURN",
+              "Clr/Home", "Run/Stop", "RESTORE", "Left Shift", "Right Shift", "Shift Lock", "SPACE", "£", "")
+DEFAULT_JOY_CONFIG = ["JU", "JD", "JL", "JR", "JF", "JF", "JF", "JF", "", "A", "B", "C", "", "", ""]
 
-VIC20BANKS = {"3 kB": ("B0"), "8 kB": ("B1"), "16 kB": ("B1", "B2"), "24 kB": ("B1", "B2", "B3"),
+VIC20BANKS = {"3 kB": ("B0",), "8 kB": ("B1",), "16 kB": ("B1", "B2"), "24 kB": ("B1", "B2", "B3"),
               "32 kB": ("B1", "B2", "B3", "B5"), "35 kB": ("B0", "B1", "B2", "B3", "B5")}
+
+
+class JoyConfigForm(QDialog, Ui_JoyConfigDialog):
+    def __init__(self, joy_num: int, config: list[str]):
+        super().__init__()
+        self.setupUi(self)
+        self.config = config.copy()
+        self.joy_num = joy_num
+        self.setWindowTitle(f"Joystick configuration {joy_num}")
+        for i in range(len(config)):
+            o = self.findChild(QtWidgets.QComboBox, f"comboBox_{i + 1}")
+            o.addItems(JOY_LABELS)
+            o.setCurrentIndex(JOY_KEYS.index(config[i]))
+
+    def accept(self):
+        for i in range(len(self.config)):
+            o = self.findChild(QtWidgets.QComboBox, f"comboBox_{i + 1}")
+            self.config[i] = JOY_KEYS[o.currentIndex()]
+        super().accept()
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -61,6 +80,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.extension = ""
         self.original = ""
         self.cjm_type = True
+        self.joy_config = [DEFAULT_JOY_CONFIG.copy(), DEFAULT_JOY_CONFIG.copy(), DEFAULT_JOY_CONFIG.copy(),
+                           DEFAULT_JOY_CONFIG.copy()]
         self.form_change("")
 
     def file_exit(self, value):
@@ -105,7 +126,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.ramsetting.setCurrentIndex(0)
 
     def joystick_config(self):
-        pass
+        joy_num = int(self.sender().objectName()[-1])
+        dlg = JoyConfigForm(joy_num, self.joy_config[joy_num - 1])
+        if dlg.exec():
+            self.joy_config[joy_num-1]=dlg.config
+            self.form_change("")
 
     def form_change(self, value) -> None:
         self.fileName.setText(self.get_filename())
@@ -123,7 +148,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if self.mouse.currentIndex() > 0:
             flags += MOUSE[self.mouse.currentIndex()]
         if self.diskmode.currentIndex() > 0:
-            flags += DISKMODE[self.diskmode.currentIndex()]
+            flags += DISK_MODE[self.diskmode.currentIndex()]
         if self.reu.currentIndex() > 0:
             flags += REU[self.reu.currentIndex()]
 
@@ -133,12 +158,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         result = self.basename
         if flags != "":
-            result += "_" + flags
+            result += f"_{flags}"
         result += self.extension
         return result
 
     def get_cjm(self) -> str:
-        result = ""
         tmp = []
         if self.model.currentIndex() > 0:
             tmp.append(("", "64", "vic")[self.model.currentIndex()])
@@ -166,9 +190,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             tmp.append("bank5")
         while "" in tmp:
             tmp.remove("")
-        if len(tmp) > 0:
-            result = "X:" + ",".join(tmp) + "\n"
-
+        result = "X:" + ",".join(tmp) + "\n" if tmp else ""
+        for i in range(4 if self.setmorejoy.isChecked() else 2):
+            tmp = self.joy_config[i][:-1]
+            result += f"J:{i + 1}{'*' if self.joystick.currentIndex()==i+1 else ''}:{','.join(tmp)}\n"
         return result
 
     def clear_form(self) -> None:
@@ -192,8 +217,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self.joystick.setCurrentIndex(JOY.index(flag))
             elif flag in MOUSE:
                 self.mouse.setCurrentIndex(MOUSE.index(flag))
-            elif flag in DISKMODE:
-                self.diskmode.setCurrentIndex(DISKMODE.index(flag))
+            elif flag in DISK_MODE:
+                self.diskmode.setCurrentIndex(DISK_MODE.index(flag))
             elif flag in REU:
                 self.reu.setCurrentIndex(REU.index(flag))
             elif flag in self.chbxs:
